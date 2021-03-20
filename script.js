6e07a7ab3b9fd95eec7d0e9fa07c593f14c9f1a7
etown285 <78112738+etown285@users.noreply.github.com>
2021-03-19 21:21:32 -0400

    added local storage

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var searchBar = document.querySelector('#searchbar');
 var searchButton = document.querySelector('#searchbutton');
+var previousSearches = document.querySelector('#searches');
 
 var API_KEY = 'ef1c4875324d0e3c275f2fe690d96608';
 
@@ -9,7 +10,7 @@ searchButton.addEventListener("click", function() {
     var userLocation = searchBar.value;
     console.log(userLocation);
     // Make our API call 
-    var url = `http://api.openweathermap.org/data/2.5/weather?q=${userLocation}&appid=${API_KEY}`;
+    var url = `http://api.openweathermap.org/data/2.5/weather?q=${userLocation}&appid=${API_KEY}&units=imperial`;
     console.log(url)
 
     fetch(url)
@@ -22,6 +23,13 @@ searchButton.addEventListener("click", function() {
 
     // Save User Query to Local Storage
 
+    searchHistory [""];
+
+    for (i = 0; i < userLocation.length; i++) {
+        var cities = userLocation[i];
+        window.localStorage.setItem(cities, JSON.stringify(searchHistory));
+    }
+
     // query for data in localStorage
 
     // convert it to JS
@@ -30,3 +38,6 @@ searchButton.addEventListener("click", function() {
 
     // Resave it back into localStorage
 });
+
+//5-Day forecast 
+
